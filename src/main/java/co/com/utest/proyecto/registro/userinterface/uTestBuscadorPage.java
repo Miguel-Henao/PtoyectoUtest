a287e7686a6bc8d09f673af8bfe459c7620d6e9d
Miguel A Henao <mghenao95@gmail.com>
2021-04-17 19:46:54 -0500

    Update uTestBuscadorPage.java

--- src/main/java/co/com/utest/proyecto/registro/userinterface/uTestBuscadorPage.java
+++ src/main/java/co/com/utest/proyecto/registro/userinterface/uTestBuscadorPage.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class uTestBuscadorPage {
-    public static final Target BUTTON_SC = Target.the("").located(By.className("ntux-nav-header-search__icon"));
-    public static final Target NOMBRE_CURSO = Target.the("").located(By.id("searchInput"));
+    public static final Target BUTTON_SC = Target.the("Search").located(By.className("ntux-nav-header-search__icon"));
+    public static final Target NOMBRE_CURSO = Target.the("Swrite the name of the course").located(By.id("searchInput"));
 
 }
